1280082438392e1644abf7a662ca6dfc633e9931
Jason Rushton <github.com@schtuph.com>
2014-05-13 12:57:18 -0600

    prerender all partial views to the index view

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,39 @@
-/*
- * GET home page.
- */
+ "use strict";
+
 
 exports.index = function(req, res) {
-    res.render('index');
+	const fs = require('fs');
+	const path = require('path');
+	const async = require('async');
+
+	const partialsPath = path.join(process.cwd(), 'views', 'partials');
+
+	fs.readdir(partialsPath, function(err, files) {
+		async.concat(
+			files,
+			__renderPartial,
+			function(err, partials) {
+				res.render('index', {
+					partials: partials
+				});
+			}
+		);
+	});
+
+
+
+	function __renderPartial(file, callback) {
+		let baseName = path.basename(file, '.jade');
+		let renderPath = 'partials/' + baseName;
+
+		res.render(renderPath, function(err, partialHtml) {
+			callback(null, {renderPath: renderPath, html: partialHtml});
+		});
+	}
+
 };
 
 exports.partial = function(req, res) {
-    var name = req.params.name;
-    res.render('partials/' + name);
+	var name = req.params.name;
+	res.render('partials/' + name);
 };
